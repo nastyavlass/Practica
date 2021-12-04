b7d8411f5e6c6a68fe15cb6a0c0d53cb2ce59079
Anastasia <yfcnzvfcz@mail.ru>
2021-12-04 09:39:14 +0300

    сделала упражнение 2 из задания 4

--- WinDB/Form1.cs
+++ WinDB/Form1.cs
@@ -31,5 +31,11 @@ namespace WinDB
             myReader.Close();
             oleDbConnection1.Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 Exercise2 = new Form2();
+            Exercise2.Show();
+        }
     }
 }
---
+++ WinDB/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinDB
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oleDbDataAdapter1.Fill(rbProductDataSet1.Поставщики);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            oleDbDataAdapter1.Update(rbProductDataSet1);
+        }
+    }
+}
